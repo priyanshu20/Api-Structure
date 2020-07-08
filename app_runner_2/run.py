c943ebecb71f932d4bba9366d73729240bfbd156
Priyanshu Sharma <59313375+priyanshu20@users.noreply.github.com>
2020-07-08 12:05:45 +0530

    Update run.py

--- app_runner_2/run.py
+++ app_runner_2/run.py
@@ -1,6 +1,8 @@
 from app_structure_2 import create_app
 
 app=create_app()
-#During product a wsgi.py file is created similar to this which is server to gunicorn etc.
+
+#for production conventionally a wsgi.py file is create which works similar to this, it serves the app object to a gunicorn server
+
 if __name__ =='__main__':
     app.run()
